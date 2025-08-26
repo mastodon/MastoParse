49add2d60f6a06f948b72ef2d74b926fe0723043
whattherestimefor <shannon@joinmastodon.org>
2025-08-26 11:05:38 +0100

    Remove obsolete comment

--- Sources/MastoParse/MastoParseHtmlTree.swift
+++ Sources/MastoParse/MastoParseHtmlTree.swift
@@ -39,7 +39,6 @@ struct ElementNode {
 private func transform(node: SwiftSoup.Node) throws -> MastoParseNode? {
     switch node {
     case let textNode as SwiftSoup.TextNode:
-        // Collapse any extra carriage returns
         let text = textNode.getWholeText()
         return text.isEmpty ? nil : .text(text)
         
